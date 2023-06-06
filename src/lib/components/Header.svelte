b89d65b5503bcdab2f16330fc49da05848d5920f
Dmytrii Puzyr <slymahel@gmail.com>
2023-06-06 20:37:48 +0300

    Update logo to be a link

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,5 @@
 <header class="bg-white px-4 py-2">
-	<span class="select-none font-extrabold">jazzguar 📸🐆🎶</span>
+	<a href="/">
+		<span class="select-none font-extrabold">jazzguar 📸🐆🎶</span>
+	</a>
 </header>
