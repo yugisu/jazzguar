cdc77c3a1af68a96ed6f7f5fe2abc21c45844c61
Dmytrii Puzyr <slymahel@gmail.com>
2023-06-08 10:26:20 +0300

    Switch to HSL color generation algorithm generated by AI

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -1,10 +1,10 @@
 export const stringToHue = (str: string) => {
-	let hash = 0;
-	for (let i = 0; i < str.length; i++) {
-		hash = str.charCodeAt(i) + ((hash << 5) - hash);
-		hash = hash & hash;
-	}
-	return hash % 360;
+	const bytes = Array.from(str.toString().toLowerCase());
+
+	const sum = bytes.reduce((a, b) => a + b.charCodeAt(0), 0);
+	const product = bytes.reduce((a, b) => a * b.charCodeAt(0), 1);
+
+	return Math.abs(sum - product) % 360;
 };
 
 export const stringToHsl = (str: string, saturation = 100, lightness = 75) => {
