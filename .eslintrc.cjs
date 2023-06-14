27aab89158a896be14b74f18bce18fc9c7797051
Dmytrii Puzyr <slymahel@gmail.com>
2023-06-14 22:17:36 +0300

    Add import sorting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,23 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:svelte/recommended', 'prettier'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:import/typescript',
+		'plugin:svelte/recommended',
+		'prettier',
+	],
 	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint', 'prettier'],
+	plugins: ['@typescript-eslint', 'prettier', 'import'],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
 		extraFileExtensions: ['.svelte'],
+	},
+	settings: {
+		'import/resolver': {
+			typescript: true,
+		},
 	},
 	env: {
 		browser: true,
@@ -14,7 +25,30 @@ module.exports = {
 		node: true,
 	},
 	rules: {
+		/// Prettier
 		'prettier/prettier': 'warn',
+
+		/// Import
+		'import/first': 'error',
+		'import/newline-after-import': 'warn',
+		'import/order': [
+			'error',
+			{
+				alphabetize: {
+					caseInsensitive: true,
+					order: 'asc',
+				},
+				groups: ['unknown', 'builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+				pathGroups: [
+					{
+						pattern: '$**/**',
+						group: 'internal',
+						position: 'before',
+					},
+				],
+				'newlines-between': 'always',
+			},
+		],
 	},
 
 	overrides: [
