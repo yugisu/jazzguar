1c04a09589bf8e1b0fade47d7d0618177835220a
Dmytrii Puzyr <slymahel@gmail.com>
2023-06-14 23:45:05 +0300

    Improve photo upload performance

--- src/lib/photos.ts
+++ src/lib/photos.ts
@@ -1,4 +1,4 @@
-import { doc, setDoc, updateDoc } from 'firebase/firestore';
+import { doc, setDoc } from 'firebase/firestore';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { Image } from 'image-js';
 import { ulid } from 'ulid';
@@ -26,30 +26,34 @@ export const resizePhoto = async (file: File): Promise<Blob | undefined> => {
 };
 
 export const savePhoto = async ({ file, tags }: { file: File; tags: string[] }) => {
-	const objectId = ulid();
+	const photoId = ulid();
 
-	const fileRef = ref(storage, `photos/general/${objectId}`);
-	await uploadBytes(fileRef, file, { cacheControl: 'max-age=604800, immutable' });
+	const fileRef = ref(storage, `photos/general/${photoId}`);
+	const thumbnailFileRef = ref(storage, `photos/general/thumbnail-${photoId}`);
 
-	const docRef = doc(photosCol(), objectId);
+	const [src, srcOptimized] = await Promise.all([
+		(async () => {
+			await uploadBytes(fileRef, file, { cacheControl: 'max-age=604800, immutable' });
+			return getDownloadURL(fileRef);
+		})(),
+		(async () => {
+			const resizedFile = await resizePhoto(file);
+			if (resizedFile) {
+				await uploadBytes(thumbnailFileRef, resizedFile, { cacheControl: 'max-age=604800, immutable' });
+				return getDownloadURL(thumbnailFileRef);
+			}
+			return undefined;
+		})(),
+	]);
 
+	const docRef = doc(photosCol(), photoId);
 	await setDoc(docRef, {
-		id: objectId,
+		id: photoId,
 		name: file.name,
-		src: await getDownloadURL(fileRef),
 		tags,
+		src,
+		...(srcOptimized && { srcOptimized }),
 	});
 
-	const resizedFile = await resizePhoto(file);
-
-	if (resizedFile) {
-		const thumbnailFileRef = ref(storage, `photos/general/thumbnail-${objectId}`);
-		await uploadBytes(thumbnailFileRef, resizedFile, { cacheControl: 'max-age=604800, immutable' });
-
-		await updateDoc(docRef, {
-			srcOptimized: await getDownloadURL(thumbnailFileRef),
-		});
-	}
-
-	await fetch(`/api/annotate/${objectId}`, { method: 'POST' });
+	await fetch(`/api/annotate/${photoId}`, { method: 'POST' });
 };
