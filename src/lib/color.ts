64ade32be2423f45b01f9ebd24f54a5e4c861b4c
Dmytrii Puzyr <slymahel@gmail.com>
2023-06-19 15:33:30 +0300

    Update the tag color generation algorithm

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -1,10 +1,17 @@
 export const stringToHue = (str: string) => {
+	let hash = 0;
+	if (str.length === 0) return hash;
+	for (let i = 0; i < str.length; i++) {
+		hash = str.charCodeAt(i) + ((hash << 5) - hash);
+		hash = hash & hash;
+	}
+
 	const bytes = Array.from(str.toString().toLowerCase());
 
 	const sum = bytes.reduce((a, b) => a + b.charCodeAt(0), 0);
 	const product = bytes.reduce((a, b) => a * b.charCodeAt(0), 1);
 
-	return Math.abs(sum - product) % 360;
+	return (hash + Math.abs(sum - product)) % 360;
 };
 
 export const stringToHsl = (str: string, saturation = 100, lightness = 75) => {
