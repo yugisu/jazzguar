e73374269c9bf3993c17a02b316f990d69224036
Dmytrii Puzyr <slymahel@gmail.com>
2023-07-04 19:13:16 +0300

    Fix an issue with firebase-admin initialization during dev

--- src/lib/firebase/admin.server.ts
+++ src/lib/firebase/admin.server.ts
@@ -6,6 +6,6 @@ if (env.GOOGLE_APPLICATION_CREDENTIALS) {
 	process.env['GOOGLE_APPLICATION_CREDENTIALS'] ??= env.GOOGLE_APPLICATION_CREDENTIALS;
 }
 
-const admin = firebaseAdmin.initializeApp();
+const admin = firebaseAdmin.apps.length === 0 ? firebaseAdmin.initializeApp() : firebaseAdmin.app();
 
 export const auth = admin.auth();
